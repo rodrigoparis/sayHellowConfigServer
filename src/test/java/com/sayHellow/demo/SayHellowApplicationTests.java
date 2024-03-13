46ee3f87faa6f625f4e3c030a74d796d25b4ec4e
rodrigo.paris <rparis@k12.com>
2024-03-13 09:26:22 -0300

    adding Spring Initlzr project

---
+++ src/test/java/com/sayHellow/demo/SayHellowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sayHellow.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SayHellowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
